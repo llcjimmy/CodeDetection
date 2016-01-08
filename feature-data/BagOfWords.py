c8ddacb6d3169ea4e966561d982b0b4d6f583b81
llcjimmy <jimmylinju@163.com>
2016-01-08 15:23:47 +0800

    first commit

---
+++ feature-data/BagOfWords.py
@@ -0,0 +1,83 @@
+import nltk
+import math
+
+def co():
+    dict = {}
+    idfDic = {}
+    numOfDoc = 0
+    globalDict = nltk.FreqDist()
+    index = 1
+    f1 = open('QuestionsBothProcessed.txt', 'r')
+    for line in f1:
+        words = line.split()
+        [globalDict.inc(word) for word in words]
+        numOfDoc += 1
+    print len(globalDict)
+
+    for key in globalDict:
+        if globalDict[key] > 1000: #10000 for SO, 100 for math
+            dict[key] = index
+            idfDic[key] = 0;
+            index += 1
+    print len(dict)
+    #print dict.keys()
+
+    f1 = open('QuestionsBothProcessed.txt', 'r')
+    for line in f1:
+        words = line.split()
+        localDict = nltk.FreqDist()
+        [localDict.inc(word) for word in words]
+        for word in localDict:
+            if word in idfDic:
+                idfDic[word] += 1
+
+    f1 = open('QuestionsBothProcessed.txt', 'r')
+    f2 = open('QuestionsFeature.txt', 'w')
+
+    index_line = 1
+    for line in f1:
+        words = line.split()
+        localDict = nltk.FreqDist()
+        [localDict.inc(word) for word in words]
+        for word in localDict:
+            if word in dict:
+                f2.write(str(index_line) + " " + str(dict[word]) + " " + str(localDict[word] / len(words) * math.log(numOfDoc / idfDic[word])))
+                f2.write("\n")
+        index_line += 1
+
+def tag():
+    dict = {}
+    globalDict = nltk.FreqDist()
+    index = 1
+    f1 = open('QuestionsTags_removeLowNumTags.txt', 'r')
+    for line in f1:
+        words = line.split()
+        [globalDict.inc(word) for word in words]
+    print len(globalDict)
+
+    for key in globalDict:
+        #print key
+        dict[key] = index
+        index += 1
+    print len(dict)
+    #print dict.keys()
+
+    f1 = open('QuestionsTags_removeLowNumTags.txt', 'r')
+    f2 = open('QuestionsLabel.txt', 'w')
+
+    index_line = 1
+    for line in f1:
+        words = line.split()
+        localDict = nltk.FreqDist()
+        [localDict.inc(word) for word in words]
+        for word in localDict:
+            if word in dict:
+                f2.write(str(index_line) + " " + str(dict[word]) + " 1")
+                f2.write("\n")
+        index_line += 1
+
+
+
+if __name__ == '__main__':
+    co()
+    tag()
